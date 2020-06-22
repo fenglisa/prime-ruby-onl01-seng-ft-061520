dc814436ca0e83d67bb0208c0ccf25ab046788bf
Lisa Feng <lisafeng10@gmail.com>
2020-06-22 14:32:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,14 @@ require "pry"
 def prime?(number)
   if number == 2
     true
+  elsif number == 3
+    i = (2..(number - 1)).to_a
+      i.each do |divisor|
+        if number % divisor == 0
+          false
+        end
+      end
+      true
   elsif number == 1||-1||0
     false
   elsif number < -1
